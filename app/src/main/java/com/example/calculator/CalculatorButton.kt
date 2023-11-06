fc6d2d60190f3e6ce63e4adb25193e559edffbe9
aryanraj0410 <ranjanayush287@gmail.com>
2023-11-07 00:04:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/CalculatorButton.kt
@@ -0,0 +1,56 @@
+package com.example.calculator
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calculator.ui.theme.Mint
+
+@Composable
+fun CalculatorButton(
+    symbol : String,
+    modifier : Modifier,
+    onClick: () -> Unit
+){
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .clickable { onClick() }
+            .then(modifier)
+    ){
+        Text(
+            text = symbol,
+            fontSize = 36.sp,
+            color = Color.White
+        )
+    }
+}
+@Composable
+fun ActionButton(
+    symbol : String,
+    modifier : Modifier,
+    onClick: () -> Unit
+){
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .clickable { onClick() }
+            .then(modifier)
+    ){
+        Text(
+            text = symbol,
+            fontSize = 36.sp,
+            color = Mint
+        )
+    }
+}
